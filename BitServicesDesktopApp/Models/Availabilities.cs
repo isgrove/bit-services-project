08b4d8fe71083ce9120020133c59f481f55b6b47
isgrove <samis@hey.com>
2022-05-01 17:54:17 +1000

    Added availability and availabilities models

---
+++ BitServicesDesktopApp/Models/Availabilities.cs
@@ -0,0 +1,43 @@
+﻿using BitServicesDesktopApp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitServicesDesktopApp.Models
+{
+    public class Availabilities : List<Availability>
+    {
+        public Availabilities()
+        {
+            SQLHelper helper = new SQLHelper();
+            string sql = "SELECT availability_date, client_id" +
+                " FROM contractor_availability";
+            DataTable availabilityTable = helper.ExecuteSQL(sql);
+            foreach (DataRow dr in availabilityTable.Rows)
+            {
+                Availability newAvailability = new Availability(dr);
+                this.Add(newAvailability);
+            }
+        }
+        public Availabilities(int contractorId)
+        {
+            SQLHelper db = new SQLHelper();
+            string sql = "SELECT availability_date, client_id" +
+                " FROM contractor_availability" +
+                " WHERE contractor_id = @ContractorId";
+            SqlParameter[] objParams = new SqlParameter[1];
+            objParams[0] = new SqlParameter("@ContractorId", DbType.Int32);
+            objParams[0].Value = contractorId;
+            DataTable availabilityTable = db.ExecuteSQL(sql, objParams);
+            foreach (DataRow dr in availabilityTable.Rows)
+            {
+                Availability newAvailability = new Availability(dr);
+                this.Add(newAvailability);
+            }
+        }
+    }
+}
---
+++ BitServicesDesktopApp/Models/Availability.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitServicesDesktopApp.Models
+{
+    public class Availability
+    {
+        private DateTime _availabilityDate;
+        private int _clientId;
+
+        public DateTime AvilabilityDate
+        {
+            get { return _availabilityDate; }
+            set { _availabilityDate = value; }
+        }
+
+        public int ClientId
+        {
+            get { return _clientId; }
+            set { _clientId = value; }
+        }
+
+        public Availability()
+        {
+
+        }
+        public Availability(DataRow dr)
+        {
+            this.ClientId = Convert.ToInt32(dr["client_id"].ToString());
+            this.AvilabilityDate = Convert.ToDateTime(dr["availability_date"].ToString());
+        }
+    }
+}
