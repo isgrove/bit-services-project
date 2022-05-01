fe75b1c70d3ba1bc0f11090854d1f53573506079
isgrove <samis@hey.com>
2022-05-01 18:03:34 +1000

    Added an override method for skills to get all skills for a specific contractor

--- BitServicesDesktopApp/Models/Skills.cs
+++ BitServicesDesktopApp/Models/Skills.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace BitServicesApp.Models
@@ -14,6 +15,22 @@ namespace BitServicesApp.Models
             string sql = "SELECT skill_name" +
                 " FROM skill";
             DataTable skillTable = helper.ExecuteSQL(sql);
+            foreach (DataRow dr in skillTable.Rows)
+            {
+                Skill newSkill = new Skill(dr);
+                this.Add(newSkill);
+            }
+        }
+        public Skills(int contractorId)
+        {
+            SQLHelper db = new SQLHelper();
+            string sql = "SELECT skill_name, contractor_id" +
+                 " FROM contractor_skill" +
+                 " WHERE contractor_id = @ContractorId";
+            SqlParameter[] objParams = new SqlParameter[1];
+            objParams[0] = new SqlParameter("@ContractorId", DbType.Int32);
+            objParams[0].Value = contractorId;
+            DataTable skillTable = db.ExecuteSQL(sql, objParams);
             foreach (DataRow dr in skillTable.Rows)
             {
                 Skill newSkill = new Skill(dr);
