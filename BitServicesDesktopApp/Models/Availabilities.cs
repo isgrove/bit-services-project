944fe3f307bb201e37e6edf4537fe69fe55fa6ed
isgrove <samis@hey.com>
2022-05-04 02:35:44 +1000

    Changed SELECT to only get availabilities that are today or in the future

--- BitServicesDesktopApp/Models/Availabilities.cs
+++ BitServicesDesktopApp/Models/Availabilities.cs
@@ -14,8 +14,9 @@ namespace BitServicesDesktopApp.Models
         public Availabilities()
         {
             SQLHelper helper = new SQLHelper();
-            string sql = "SELECT availability_date, client_id" +
-                " FROM contractor_availability";
+            string sql = "SELECT availability_date, contractor_id" +
+                " FROM contractor_availability" +
+                " WHERE availability_date >= CONVERT(char(10), GetDate(),126)";
             DataTable availabilityTable = helper.ExecuteSQL(sql);
             foreach (DataRow dr in availabilityTable.Rows)
             {
@@ -26,9 +27,10 @@ namespace BitServicesDesktopApp.Models
         public Availabilities(int contractorId)
         {
             SQLHelper db = new SQLHelper();
-            string sql = "SELECT availability_date, client_id" +
+            string sql = "SELECT availability_date, contractor_id" +
                 " FROM contractor_availability" +
-                " WHERE contractor_id = @ContractorId";
+                " WHERE contractor_id = @ContractorId" +
+                " AND availability_date >= CONVERT(char(10), GetDate(),126)";
             SqlParameter[] objParams = new SqlParameter[1];
             objParams[0] = new SqlParameter("@ContractorId", DbType.Int32);
             objParams[0].Value = contractorId;
