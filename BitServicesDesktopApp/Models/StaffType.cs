3b79b3b50aac4320698ab972e10ca8f61c011a98
isgrove <samis@hey.com>
2022-05-04 04:23:24 +1000

    Created StaffType model

---
+++ BitServicesDesktopApp/Models/StaffType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitServicesDesktopApp.Models
+{
+    public class StaffType : INotifyPropertyChanged
+    {
+        private string _type;   
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set
+            {
+                _type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        public StaffType()
+        {
+
+        }
+        public StaffType(DataRow dr)
+        {
+            this.Type = dr["type"].ToString();
+        }
+    }
+}
---
+++ BitServicesDesktopApp/Models/StaffTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitServicesDesktopApp.DAL;
+
+namespace BitServicesDesktopApp.Models
+{
+    public class StaffTypes : List<StaffType>
+    {
+        public StaffTypes()
+        {
+            SQLHelper helper = new SQLHelper();
+            string sql = "SELECT type" +
+                         " FROM staff_type";
+            DataTable staffTable = helper.ExecuteSQL(sql);
+            foreach (DataRow dr in staffTable.Rows)
+            {
+                StaffType newStaffType = new StaffType(dr);
+                this.Add(newStaffType);
+            }
+        }
+    }
+}
