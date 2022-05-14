326ec8749340e092af79be185a8f6b32cb6041dc
isgrove <samis@hey.com>
2022-05-14 18:19:28 +1000

    Refactored CompletedJobs to work with remodeled database

--- BitServicesWebApp/BLL/CompletedJobs.cs
+++ BitServicesWebApp/BLL/CompletedJobs.cs
@@ -13,14 +13,8 @@ namespace BitServicesWebApp.BLL
         public DataTable AllCompletedJobs()
         {
             SQLHelper helper = new SQLHelper();
-            string sql = "SELECT c.client_name + ' ' + l.suburb AS [Client Location], co.first_name + ' ' + co.last_name AS [Contractor Name]," +
-                         " j.description AS[Description], format(j.deadline_date, 'D') AS[Deadline Date], 'To Implement' AS[Completed Date], j.job_id" +
-                         " FROM job j " +
-                         " INNER JOIN client_location l ON l.location_id = j.location_id" +
-                         " INNER JOIN client c ON c.client_id = l.client_id" +
-                         " INNER JOIN contractor co ON co.contractor_id = j.assigned_contractor_id" +
-                         " WHERE j.job_status = 'Completed'";
-            DataTable jobsTable = helper.ExecuteSQL(sql);
+            string sql = "usp_GetCompletedJobs";
+            DataTable jobsTable = helper.ExecuteSQL(sql, null, true);
             return jobsTable;
 
         }
